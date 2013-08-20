0242fde2672ae5e09c4181a8d3e8c253d8ce6010
alexaylwin <alexaylwin@gmail.com>
2013-08-20 12:25:29 -0400

    Extracted bacms.ca files
    - Moved bacms.ca files to a separate project
    - Fixed gitignore which was ignoing bac admin panel files

---
+++ bac/admin/index.php
@@ -0,0 +1,32 @@
+<?php
+include 'auth.php';
+include '../src/framework/classloader.php';
+?>
+
+<?php
+include 'header.php'
+?>
+<h4>
+	Admin Home
+</h4>
+<p>
+	Welcome back to your BAC Administration panel. From here, you can edit your sitemap, update your container content or change
+	other administrative settings.
+	<br />
+</p>
+Administrative tasks:
+<ul class="unstyled">
+	<li>
+		<a href="pages.php">Edit a page</a>
+	</li>
+	<li>
+		<a href="setup.php">Change administrative settings or edit site map</a>
+	</li>
+	<li>
+		<a href="logout.php">Log out</a>
+	</li>
+</ul>
+
+<?php
+include 'footer.php'
+?>
