f8436ace99a0d57c7017cf47908d8b48f2639c36
alexaylwin <alexaylwin@gmail.com>
2013-08-20 10:32:07 -0400

    updated gitignore to remove bacms.ca

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" = "clean" ]; then
-	rm -rf /z/Code/www/barelycms/*
-fi
-if [ "$1" = "cleanpages" ]; then
-	rm -rf /z/Code/www/barelycms/bac/container_content/*
-fi
-cp /z/Code/Aptana/barelycms/* /z/Code/www/barelycms  -f -R -v -u
